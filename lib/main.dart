a4a5551623812592b24f261ee8535096d3a45237
mirnaembaby <mirnaembaby@gmail.com>
2025-01-03 17:34:09 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Text('Flutter Demo Home Page'),
+      home: Column(),
     );
   }
 }
